887010d5b7cfe874559c15cafdc208159947ce0e
Mikey <mikeyzeitz@gmail.com>
2013-04-24 23:59:00 -0400

    Adding lean to turn. I don't think I actually changed anything in the scene

--- Assets/Our Scripts/changeLanes.js
+++ Assets/Our Scripts/changeLanes.js
@@ -1,5 +1,6 @@
 var bike : GameObject;
 var lanePos : String;
+var rotationAmount : float;
 
 function Start () 
 {	
@@ -7,24 +8,49 @@ function Start ()
 	{
     	bike = GameObject.FindGameObjectWithTag ('bike');
 	}
+	rotationAmount = 0.5;
 }
 
 function Update () 
 {
 	if(Input.GetKey('a'))
 	{
-		if (bike.transform.position.x > 0)
+		if (bike.transform.position.x > -0.5) // Turn Left
 		{
 			bike.transform.Translate(Vector3.left *1000* Time.deltaTime);
+			if (bike.transform.rotation.z < rotationAmount) {
+				bike.transform.rotation.z += (rotationAmount * Time.deltaTime);
+				bike.transform.position.y = 0.5;
+			}
+		} else if (bike.transform.rotation.z > 0.01){ // Already as far left as possible
+			bike.transform.rotation.z -= (rotationAmount * Time.deltaTime);
+			bike.transform.position.y = 0.5;
 		}
 	}
 	
-	if(Input.GetKey('d'))
+	else if(Input.GetKey('d'))
 	{
-		if (bike.transform.position.x < 340)
+		if (bike.transform.position.x < 340) // Turn right
 		{
 			bike.transform.Translate(Vector3.right *1000* Time.deltaTime);
+			if (bike.transform.rotation.z > -1 * rotationAmount) {
+				bike.transform.rotation.z -= (rotationAmount * Time.deltaTime);
+				bike.transform.position.y = 0.5;
+			}
+		} else if (bike.transform.rotation.z < -0.01) { // Alreaady as far right as possible
+			bike.transform.rotation.z += (rotationAmount * Time.deltaTime);
+			bike.transform.position.y = 0.5;
 		}
+	}
+	
+	else if (bike.transform.rotation.z > 0.01){
+			bike.transform.rotation.z -= (rotationAmount * Time.deltaTime);
+			bike.transform.position.y = 0.5;
+	}
+	
+	else if (bike.transform.rotation.z < -0.01) {
+			bike.transform.rotation.z += (rotationAmount * Time.deltaTime);
+			bike.transform.position.y = 0.5;
 	}
 }
 
