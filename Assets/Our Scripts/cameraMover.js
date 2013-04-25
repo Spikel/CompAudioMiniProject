fdf09688643816722988798755873ab2b7be2b29
Andrew Darrohn <a_darrohn@yahoo.com>
2013-04-24 23:59:00 -0400

    going down road

--- Assets/Our Scripts/cameraMover.js
+++ Assets/Our Scripts/cameraMover.js
@@ -13,5 +13,10 @@ function Start ()
 function Update () 
 {
 	bike.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	
+	if (bike.transform.position.z >= 72300)
+	{
+		bike.transform.rotation = Quaternion.Euler(0, 20.6, 0);
+	}
 }
 
