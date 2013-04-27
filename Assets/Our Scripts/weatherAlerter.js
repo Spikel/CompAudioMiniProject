385c97dfaea5580854c08b4dc4624745015a34e4
Clay Garrett <claygarrett@gmail.com>
2013-04-26 21:31:50 -0400

    no message

--- Assets/Our Scripts/weatherAlerter.js
+++ Assets/Our Scripts/weatherAlerter.js
@@ -4,7 +4,9 @@ private var hasPlayedWeather = false;
 private var hasPlayedGPS = false;
 public var weatherAlertSound : AudioSource;
 public var gpsSound : AudioSource;
-
+public var severeFlooding : AudioSource;
+public var highWinds : AudioSource;
+public var severeThunderstorms : AudioSource;
 
 function Start () {
 
@@ -14,12 +16,26 @@ function Update () {
 	var bike = this;
 	
 	if(bike.transform.position.z > 62000 && !hasPlayedGPS) {
-		gpsSound.Play();
-		hasPlayedGPS = true;
+		//gpsSound.Play();
+		//hasPlayedGPS = true;
+		
+		var rand = Random.Range(0, 2);
+		
+		switch(rand) {
+			case 0:
+				severeFlooding.Play();
+				break;
+			case 1:
+				highWinds.Play();
+				break;
+			case 2:
+				severeThunderstorms.Play();
+				break;
+		}
 		
 	} else if(bike.transform.position.z > 60000 && !hasPlayedWeather) {
-		weatherAlertSound.Play();
-		hasPlayedWeather = true;
+	//	weatherAlertSound.Play();
+	//	hasPlayedWeather = true;
 		
 	}
 	
